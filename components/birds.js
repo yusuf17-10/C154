43f31d848d5ea303cd97c768a6113e247db8f66a
yusuf17-10 <64640684+yusuf17-10@users.noreply.github.com>
2021-09-23 21:11:46 +0530

    Add files via upload

---
+++ components/birds.js
@@ -0,0 +1,29 @@
+AFRAME.registerComponent('bird', {
+    
+
+    init: function () {
+        for(var i = 1;i<=30;i++){
+            var id  = `bird${i}`
+            var posX = Math.random()*3000 - 1000
+            var posZ = Math.random()*3000 - 1000
+            var posY = Math.random()*2 - 1
+
+            var position = {x:posX,y:posY,z:posZ}
+            
+            this.createBird(id,position)
+        }
+    },
+
+
+    createBird:function(id,position){
+        var birdEl = document.createElement("a-entity")
+        birdEl.setAttribute("position",position)
+        birdEl.setAttribute("id",id)
+        birdEl.setAttribute("scale",{x:500,y:500,z:500})
+        birdEl.setAttribute("gltf-model","./assets/models/flying_bird/scene.gltf")
+        birdEl.setAttribute("animation-mixer",{})
+        var terrainEl = document.querySelector("#terrainmap")
+        terrainEl.appendChild(birdEl)
+    }
+
+});
